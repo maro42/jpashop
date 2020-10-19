21392e19875804666e43e865fc6206082218b532
maro42 <maro42@naver.com>
2020-10-19 21:30:53 +0900

    상품검색 강의 완료

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,10 +1,15 @@
 package jpabook.jpashop.repository;
 
+import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -22,19 +27,84 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-    // 주문 리스트 조회(검색조건 - 동적쿼리)
-    public List<Order> findAllByString(OrderSearch orderSearch) {
-//        return em.createQuery("select o from Order o join o.member m where o.status = :status and m.name = :name", Order.class)
-//                .setParameter("status", orderSearch.getOrderStatus())
-//                .setParameter("name", orderSearch.getMemberName())
-//                //.setFirstResult(pageIndex)    // 페이징할 경우 현재 페이지번호
-//                .setMaxResults(1000)    // 최대 1000건
-//                .getResultList();
-        // 위 주석처럼 하면 조건이 하나라도 없을 때 에러발생 -> 동적쿼리 만들어줘야한다.
 
-        String jpql = "select o from Order o join o.member m";
-        return em.createQuery(jpql,Order.class)
-                .setMaxResults(1000)    // 최대 1000건
-                .getResultList();
+    /**
+     * 동적쿼리 생성 방법
+     * 1. 쿼리를 파라미터에 따라 문자열 이어붙여 쿼리문 만들기
+     * 2. JPA Criteria(JPA 표준스팩)
+     * 3. query dsl
+     */
+
+    // 주문 리스트 조회(검색조건 - 동적쿼리)
+//    public List<Order> findAll(OrderSearch orderSearch) {
+////        return em.createQuery("select o from Order o join o.member m where o.status = :status and m.name = :name", Order.class)
+////                .setParameter("status", orderSearch.getOrderStatus())
+////                .setParameter("name", orderSearch.getMemberName())
+////                //.setFirstResult(pageIndex)    // 페이징할 경우 현재 페이지번호
+////                .setMaxResults(1000)    // 최대 1000건
+////                .getResultList();
+//        // 위 주석처럼 하면 조건이 하나라도 없을 때 에러발생 -> 동적쿼리 만들어줘야한다.
+//
+//
+//    }
+
+    public List<Order> findAllByString(OrderSearch orderSearch) {
+        //language=JPAQL
+        String jpql = "select o From Order o join o.member m";
+        boolean isFirstCondition = true;
+        //주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " o.status = :status";
+        }
+        //회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " m.name like :name";
+        }
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class)
+                .setMaxResults(1000); //최대 1000건
+        if (orderSearch.getOrderStatus() != null) {
+            query = query.setParameter("status", orderSearch.getOrderStatus());
+        }
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            query = query.setParameter("name", orderSearch.getMemberName());
+        }
+        return query.getResultList();
     }
+
+    public List<Order> findAllByCriteria(OrderSearch orderSearch) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+        Root<Order> o = cq.from(Order.class);
+        Join<Order, Member> m = o.join("member", JoinType.INNER); //회원과 조인
+        List<Predicate> criteria = new ArrayList<>();
+        //주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            Predicate status = cb.equal(o.get("status"),
+                    orderSearch.getOrderStatus());
+            criteria.add(status);
+        }
+        //회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            Predicate name =
+                    cb.like(m.<String>get("name"), "%" +
+                            orderSearch.getMemberName() + "%");
+            criteria.add(name);
+        }
+        cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+        TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000); //최대 1000건
+        return query.getResultList();
+    }
+
 }
